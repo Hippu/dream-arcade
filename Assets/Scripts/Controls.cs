ac285d87e30c88d4478b486c90bec942c8b8fd29
RoopeLehtonen <rosale@utu.fi>
2016-12-11 17:25:52 +0200

    Looks like a game
    Wide room with bed and arcade.

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movespeed;
+    public bool moveright;
+    public bool moveleft;
+
+    private Animator animator;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+
+        //For keyboard:
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+            animator.SetInteger("Direction", 2);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+            animator.SetInteger("Direction", 1);
+
+        }
+
+        //For touch controls:
+        if (moveright)
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+        if (moveleft)
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+    }
+}
